7ebb10c120e43aed24b93f5b0d6bc5074501ee48
Marcello Barnaba <vjt@openssl.it>
2009-09-07 15:44:36 +0200

    Added If-Modified-Since and Etag header handling, with 304 responses for caching purposes

--- launch/functions.php
+++ launch/functions.php
@@ -1,5 +1,29 @@
 <?php
 function stream_map_file($attachment = true) {
+  // Shamelessly inspired from drupal source includes/bootstrap.inc
+  //
+  $last_modified = gmdate("D, d M Y H:i:s", filemtime(MAPFILE)) . ' GMT';
+  $etag = '"' . md5($last_modified) . '"';
+
+  $if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? stripslashes($_SERVER['HTTP_IF_MODIFIED_SINCE']) : false;
+  $if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? stripslashes($_SERVER['HTTP_IF_NONE_MATCH']) : false;
+
+  if ($if_modified_since && $if_none_match
+    && $if_none_match == $etag
+    && $if_modified_since == $last_modified) {
+      header('HTTP/1.1 304 Not Modified');
+      header("Etag: $etag");
+      exit;
+  }
+
+  // If no cache is available, send the file
+  //
+  header("Last-Modified: $last_modified");
+  header("Etag: $etag");
+
+  header("Expires: Sun, 19 Nov 1978 05:00:00 GMT");
+  header("Cache-Control: must-revalidate");
+
   // Shamelessly copied from http://php.net/readfile
   //
   header('Content-Description: Watermap Home Print');
@@ -9,9 +33,6 @@ function stream_map_file($attachment = true) {
     ($attachment ? 'attachment; filename=' . basename(MAPFILE) : 'inline'));
 
   header('Content-Transfer-Encoding: binary');
-  //header('Expires: 0');
-  //header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-  //header('Pragma: public');
   header('Content-Length: ' . filesize(MAPFILE));
 
   ob_clean();
